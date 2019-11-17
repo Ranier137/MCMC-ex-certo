9ba612e3d834ba7e7a5cb78872c4be19ca091ba5
ranier <ranier_m@hotmail.com>
2019-11-17 17:01:11 -0300

    3d plot

--- CDM/__init__.py
+++ CDM/__init__.py
@@ -7,17 +7,17 @@ import numpy as np
 class LCDModel:
     
     #construtor das instâncias
-	def __init__(self, H0, c, OM_r, OM_m, OM_de):
+	def __init__(self, H0, c, OM_r, OM_m, OM_de, w):
 		self.H0 = H0
 		self.c = c
 		self.OM_m = abs(OM_m)
 		self.OM_de = abs(OM_de)
 		self.OM_r = abs(OM_r)
-
+		self.w = w
     #parâmetro de Hubble/H0
 	def e(self, z):
 		#OM_k = 1.0 - self.OM_r - self.OM_m - self.OM_de      
-		return math.sqrt(self.OM_r*(1.0+z)**4 + self.OM_m*(1.0+z)**3 + self.OM_de)
+		return math.sqrt(self.OM_r*(1.0+z)**4 + self.OM_m*(1.0+z)**3 + self.OM_de*(1+z)**(3*(1 + self.w)))
     
 	#comoving distance
 	def r(self, z):
--- MCMC2.py
+++ MCMC2.py
@@ -3,18 +3,21 @@ import numpy as np
 import CDM 
 import matplotlib.pyplot as plt
 from decimal import Decimal
+from mpl_toolkits.mplot3d import Axes3D
 
 #o problema e gerar uma distribuiçao no espaço dos parametros OM_m(materia) e OM_DE(dark energy) dado o modelo LCDM (usando o pacote que escrevi CDM). Os parametros sao denotados por x = [OM_m, OM_DE].
 
 #aqui defino a distribuiçao de transiçao. Pega os dois parametros, guardados no vetor x, e devolve outros dois parametros de acordo com a distribuiçao normal, centrada em cada um dos parametros iniciais:
 
-Transition = lambda x: [np.random.normal(x[0], 0.05), np.random.normal(x[1], 0.05)]
+Transition = lambda x: [np.random.normal(x[0], 0.05), np.random.normal(x[1], 0.05), np.random.normal(x[2], 0.05)]
 
 #Definir a distribuiçao prior para cada calor dos parametros armazenado no vetor x
     
 def prior(x):
 # coloquei aqui a informaçao de que a probabilidade deve se anular caso algum dos parametros se anule ou se torne negativo.
 	if x[0]<= 0. or x[1]<=0.: 
+		return 0.0
+	if x[2] > 0.0 :
 		return 0.0
 	#k = abs(1 - x[0] - x[1])
 	#if k > 0.1:
@@ -28,7 +31,7 @@ def prior(x):
 def LnLike(x,data1, data2,sig = 0.4): 
     d = len(data1) 		#número de dados coletados
     deltax = np.zeros(d)
-    M = CDM.LCDModel(72, 299792.4580, 0.0, x[0], x[1]) #é criado uma instância do modelo LCDM, fixando constante de hubble atualmente H0 = 72, c = 299792.458km/s, OM_r = 0.
+    M = CDM.LCDModel(72, 299792.4580, 0.0, x[0], x[1], x[2]) #é criado uma instância do modelo LCDM, fixando constante de hubble atualmente H0 = 72, c = 299792.458km/s, OM_r = 0.
     i=0
     Mo = -19.3	#assumindo que todas as SNIa possuem magnitude absoluta iguais
     MI2 = np.zeros(len(data2))	
@@ -105,23 +108,27 @@ while i < len(coloumn0):
 #chute inicial dos parâmetros para a cadeia
 param_init0 = float(input(print('digite um chute inicial para Omega_m: \n'))) 
 param_init1 = float(input(print('digite um chute inicial para Omega_DE: \n')))
+param_init2 = float(input(print('digite um chute inicial para w: \n')))
 iterations = int(input(print('digite o numero de iterações: \n')))    
 sig = 0.4
 
 
 
-xi = np.array([param_init0, param_init1])    #ponto inicial da cadeia
+xi = np.array([param_init0, param_init1, param_init2])    #ponto inicial da cadeia
 #lista com os valores aceitos dos parâmetros. Inicialmente há apenas um valor
 chain_m = [xi[0]]	
 chain_DE = [xi[1]]
+chain_w = [xi[2]]
 #lista de valores sorteados para os parâmetros porém rejeitados 
 chain_mrej = []
 chain_DErej = []
+chain_wrej = []
 accepted = [0] #numeros de sorteios aceitados
 rejected = [0] #numeros de sorteios rejeitados
 
 
 for i in range(iterations): #iteração i
+    print('----------------------------------------------------------------')
     print('iteração ', i, '\n') #monitoramento da contagem   
     xp = Transition(xi) #sorteio de um novo ponto, xp, a partir do ponto atual xi
     ret = Passo(xi,xp, data1, data2, sig, LnLike) # verificar a condição de aceite de xp. Note que assumirá valores 1 (aceite) ou 0 (recusa). 
@@ -135,34 +142,57 @@ for i in range(iterations): #iteração i
 	# insere na lista dos recusados xp
         chain_mrej.append(xp[0]) 
         chain_DErej.append(xp[1])
+        chain_wrej.append(xp[2])
 
     print(' true: ', accepted, '\n\n')
     print(' false: ', rejected, '\n\n')
-
+    
 #acrescenta os valores atualizados à cadeia.
     chain_m.append(xi[0])
     chain_DE.append(xi[1])
-    
+    chain_w.append(xi[2])
+    print('----------------------------------------------------------------')
+
 #após todas as iterações cálculo do valor médio    
 ValorMM = sum(chain_m)/len(chain_m)
 ValorMDE = sum(chain_DE)/len(chain_DE)
+ValorMw = sum(chain_w)/len(chain_w)
 
-print(f'\n o valor esperado dos parametros sao OM_m = {ValorMM}, OM_DE = {ValorMDE}')
+print(f'\n o valor esperado dos parametros sao OM_m = {ValorMM}, OM_DE = {ValorMDE}, ValorMw = {ValorMw}')
 
 #plotar os histogramas
 plt.hist(chain_m, bins = 100, label= 'Matter', color= 'blue')
 plt.hist(chain_DE, bins = 100, label= 'Dark Energy', color= 'red')
+plt.hist(chain_w, bins = 100, label= 'w', color= 'green')
 plt.legend()
 plt.show()
 
+'''
 #pontos percorridos no espaço de parâmetros
-plt.scatter(chain_mrej, chain_DErej, marker='x',color='y',linewidths=1) # recusados em amarelo (yellow)
-plt.scatter(chain_m, chain_DE,marker='.',color='b',linewidths=3)# aceitos em azul (blue)
+plt.scatter(chain_mrej, chain_DErej, chain_DErej,  marker='x',color='y',linewidths=1) # recusados em amarelo (yellow)
+plt.scatter(chain_m, chain_DE, chain_w, marker='.',color='b',linewidths=3)# aceitos em azul (blue)
 plt.xlabel('OM_m')
 plt.ylabel('OM_DE')
+plt.zlabel('w')
 plt.show()
 plt.title('Espaço de Parâmetros')
 		
+'''
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+ax.scatter(chain_m, chain_DE, chain_w, c='b', marker='o')
+ax.scatter(chain_mrej, chain_DErej, chain_DErej, c='y', marker='o')
+
+ax.set_xlabel('OM_m')
+ax.set_ylabel('OM_DE')
+ax.set_zlabel('w')
+plt.title('Espaço de Parâmetros')
+plt.show()
+
+
+
 
     
 '''
